3444f4c0b83d9aecfa674d06170b9e7e47b9ec0f
Yash126116 <86655251+Yash126116@users.noreply.github.com>
2024-06-10 16:35:34 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -2,20 +2,17 @@ import pymongo
 import os
 from helper.date import add_date
 from config import *
+
 mongo = pymongo.MongoClient(DB_URL)
 db = mongo[DB_NAME]
 dbcol = db["user"]
 
 # Total User
-
-
 def total_user():
     user = dbcol.count_documents({})
     return user
 
-# insert bot Data
-
-
+# Insert bot Data
 def botdata(chat_id):
     bot_id = int(chat_id)
     try:
@@ -24,73 +21,59 @@ def botdata(chat_id):
     except:
         pass
 
-
 def total_rename(chat_id, renamed_file):
     now = int(renamed_file) + 1
     dbcol.update_one({"_id": chat_id}, {"$set": {"total_rename": str(now)}})
-
 
 def total_size(chat_id, total_size, now_file_size):
     now = int(total_size) + now_file_size
     dbcol.update_one({"_id": chat_id}, {"$set": {"total_size": str(now)}})
 
-
-# insert user data
+# Insert user data
 def insert(chat_id):
     user_id = int(chat_id)
     user_det = {"_id": user_id, "file_id": None, "caption": None, "daily": 0, "date": 0,
-                "uploadlimit": 10995116277760, "used_limit": 0, "usertype": "Free", "prexdate": None}
+                "uploadlimit": 10995116277760, "used_limit": 0, "usertype": "Free", "prexdate": None,
+                "metadata": False, "metadata_code": ""}
     try:
         dbcol.insert_one(user_det)
     except:
         return True
         pass
 
-
 def addthumb(chat_id, file_id):
     dbcol.update_one({"_id": chat_id}, {"$set": {"file_id": file_id}})
-
 
 def delthumb(chat_id):
     dbcol.update_one({"_id": chat_id}, {"$set": {"file_id": None}})
 
-
 def addcaption(chat_id, caption):
     dbcol.update_one({"_id": chat_id}, {"$set": {"caption": caption}})
-
 
 def delcaption(chat_id):
     dbcol.update_one({"_id": chat_id}, {"$set": {"caption": None}})
 
-
 def dateupdate(chat_id, date):
     dbcol.update_one({"_id": chat_id}, {"$set": {"date": date}})
-
 
 def used_limit(chat_id, used):
     dbcol.update_one({"_id": chat_id}, {"$set": {"used_limit": used}})
 
-
 def usertype(chat_id, type):
     dbcol.update_one({"_id": chat_id}, {"$set": {"usertype": type}})
 
-
 def uploadlimit(chat_id, limit):
     dbcol.update_one({"_id": chat_id}, {"$set": {"uploadlimit": limit}})
-
 
 def addpre(chat_id):
     date = add_date()
     dbcol.update_one({"_id": chat_id}, {"$set": {"prexdate": date[0]}})
 
-
 def addpredata(chat_id):
     dbcol.update_one({"_id": chat_id}, {"$set": {"prexdate": None}})
 
-
 def daily(chat_id, date):
     dbcol.update_one({"_id": chat_id}, {"$set": {"daily": date}})
-
 
 def find(chat_id):
     id = {"_id": chat_id}
@@ -104,7 +87,6 @@ def find(chat_id):
 
         return [file, caption]
 
-
 def getid():
     values = []
     for key in dbcol.find():
@@ -115,6 +97,20 @@ def getid():
 def delete(id):
     dbcol.delete_one(id)
 
-
 def find_one(id):
     return dbcol.find_one({"_id": id})
+
+# Metadata functions
+def get_metadata(user_id):
+    user = dbcol.find_one({"_id": user_id})
+    return user.get("metadata", False)
+
+def set_metadata(user_id, bool_meta):
+    dbcol.update_one({"_id": user_id}, {"$set": {"metadata": bool_meta}})
+
+def get_metadata_code(user_id):
+    user = dbcol.find_one({"_id": user_id})
+    return user.get("metadata_code", "")
+
+def set_metadata_code(user_id, metadata_code):
+    dbcol.update_one({"_id": user_id}, {"$set": {"metadata_code": metadata_code}})
