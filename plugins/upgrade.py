7538922c0bf3306c6157970dea7884915fedcee6
Yash126116 <86655251+Yash126116@users.noreply.github.com>
2024-06-19 18:14:17 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -6,39 +6,12 @@ async def upgrade(bot,update):
 	text = """**Free Plan User**
 	Daily  Upload limit 10TB ~ Unlimited 
 	Price 0
+
+ **💫 Free Trial**
+ Contact Admin To Activate You Free Trial
 	
 	**🪙 Basic** 
 	Daily  Upload  limit 10GB
-	Pric 🌎 0.5$  per Month
-	
-	**⚡ Standard**
-	Daily Upload limit 50GB
-	Price 🌎 1$  per Month
-	
-	**💎 Pro**
-	Daily Upload limit 100GB
-	Price 🌎 1.5$  per Month
-	
-	
-	Pay Using Binance Pay I'd `185823678`
-	
-	After Payment Send Screenshots Of 
-        Payment To Admin @Cashscopebot"""
-	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("Admin",url = "https://t.me/Cashscopebot")], 
-        			[InlineKeyboardButton("Phone Pay",url = "https://graph.org/file/8193b7e8d969f8db28ab2.jpg"),
-        			InlineKeyboardButton("Paytm Wallet/UPI",url = "https://graph.org/file/8193b7e8d969f8db28ab2.jpg")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
-	await update.message.edit(text = text,reply_markup = keybord)
-	
-
-@Client.on_message(filters.private & filters.command(["upgrade"]))
-async def upgradecm(bot,message):
-	text = """**Free Plan User**
-	Daily  Upload limit 10TB ~ Unlimited 
-	Price 0
-	
-	**🪙 Basic** 
-	Daily  Upload  limit 20GB
 	Price 🌎 0.5$  per Month
 	
 	**⚡ Standard**
@@ -55,7 +28,40 @@ async def upgradecm(bot,message):
 	After Payment Send Screenshots Of 
         Payment To Admin @Cashscopebot"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("Admin",url = "https://t.me/Cashscopebot")], 
+        			InlineKeyboardButton("Admin",url = "https://t.me/Uaaaaaaa")], 
+        			[InlineKeyboardButton("Phone Pay",url = "https://graph.org/file/8193b7e8d969f8db28ab2.jpg"),
+        			InlineKeyboardButton("Paytm Wallet/UPI",url = "https://graph.org/file/8193b7e8d969f8db28ab2.jpg")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+	await update.message.edit(text = text,reply_markup = keybord)
+	
+
+@Client.on_message(filters.private & filters.command(["upgrade"]))
+async def upgradecm(bot,message):
+	text = """**Free Plan User**
+	Daily  Upload limit 10TB ~ Unlimited 
+	Price 0
+	
+ **💫 Free Trial**
+ Contact Admin To Activate You Free Trial
+	
+	**🪙 Basic** 
+	Daily  Upload  limit 10GB
+	Price 🌎 0.5$  per Month
+	
+	**⚡ Standard**
+	Daily Upload limit 50GB
+	Price 🌎 1$  per Month
+	
+	**💎 Pro**
+	Daily Upload limit 100GB
+	Price 🌎 1.5$  per Month
+	
+	
+	Pay Using Binance Pay I'd `185823678`
+	
+	After Payment Send Screenshots Of 
+        Payment To Admin @Cashscopebot"""
+	keybord = InlineKeyboardMarkup([[ 
+        			InlineKeyboardButton("Admin",url = "https://t.me/Uaaaaaaa")], 
         			[InlineKeyboardButton("Phone Pay",url = "https://graph.org/file/8193b7e8d969f8db28ab2.jpg"),
         			InlineKeyboardButton("Paytm Wallet/UPI",url = "https://graph.org/file/8193b7e8d969f8db28ab2.jpg")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await update.message.edit(text = text,reply_markup = keybord)
