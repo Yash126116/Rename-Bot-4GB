a462a22df3bd87aa608d41947f25b63888f2e61d
Yash126116 <86655251+Yash126116@users.noreply.github.com>
2024-06-23 18:20:00 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -18,12 +18,14 @@ log_channel = LOG_CHANNEL
 link_channel = LINK_CHANNEL
 app = Client("test", api_id=API_ID, api_hash=API_HASH, session_string=STRING)
 
+
 @Client.on_callback_query(filters.regex('cancel'))
 async def cancel(bot, update):
     try:
         await update.message.delete()
-    except:
-        return
+    except Exception as e:
+        print(e)
+
 
 @Client.on_callback_query(filters.regex('rename'))
 async def rename(bot, update):
@@ -33,33 +35,10 @@ async def rename(bot, update):
     chat_id = update.message.chat.id
     id = update.message.reply_to_message_id
     await update.message.delete()
-    await update.message.reply_text(f"__Please Enter The New Filename...__\n\nNote:- Extension Not Required", reply_to_message_id=id, reply_markup=ForceReply(True))
+    await update.message.reply_text(f"__Please Enter The New Filename...__\n\nNote:- Extension Not Required", reply_to_message_id=id,
+                                    reply_markup=ForceReply(True))
     dateupdate(chat_id, date)
 
-@Client.on_callback_query(filters.regex(r"download_(.*)"))
-async def download_file(client, callback_query):
-    file_id = callback_query.data.split("_")[1]
-    user_id = callback_query.from_user.id
-
-    # Fetch messages from link_channel
-    messages = await client.get_chat_history(link_channel)
-    download_link = None
-
-    # Look for the message containing the file_id and extract the download link
-    for msg in messages:
-        if msg.reply_markup and msg.document and msg.document.file_id == file_id:
-            for button_row in msg.reply_markup.inline_keyboard:
-                for button in button_row:
-                    if button.url and "fast_link" in button.url:
-                        download_link = button.url
-                        break
-            if download_link:
-                break
-
-    if download_link:
-        await client.send_message(user_id, f"Download your file from: [Download Link]({download_link})", parse_mode='markdown')
-    else:
-        await client.send_message(user_id, "Failed to find a download link.")
 
 @Client.on_callback_query(filters.regex("vid"))
 async def vid(bot, update):
@@ -72,18 +51,45 @@ async def vid(bot, update):
     file_path = f"downloads/{new_filename}"
     message = update.message.reply_to_message
     file = message.document or message.video or message.audio
+
+    if not file:
+        await update.message.edit("No media file found.")
+        return
+
     ms = await update.message.edit("`Trying to download...`")
     used_limit(update.from_user.id, file.file_size)
     c_time = time.time()
     total_used = used + int(file.file_size)
     used_limit(update.from_user.id, total_used)
-    try:
-        path = await bot.download_media(message=file, progress=progress_for_pyrogram, progress_args=("`Trying to download...`", ms, c_time))
-    except Exception as e:
-        neg_used = used - int(file.file_size)
-        used_limit(update.from_user.id, neg_used)
-        await ms.edit(e)
-        return
+    
+    # Download from link channel if DC is 3, 4, or 5
+    dcid = FileId.decode(file.file_id).dc_id
+    if dcid in [3, 4, 5]:
+        try:
+            link_message = await bot.get_messages(link_channel, message.id)
+            if link_message.reply_markup:
+                for row in link_message.reply_markup.inline_keyboard:
+                    for button in row:
+                        if button.url:
+                            path = await bot.download_media(button.url, progress=progress_for_pyrogram, progress_args=("`Downloading...`", ms, c_time))
+                            break
+            else:
+                await ms.edit("No valid link found.")
+                return
+        except Exception as e:
+            neg_used = used - int(file.file_size)
+            used_limit(update.from_user.id, neg_used)
+            await ms.edit(f"Failed to fetch link: {str(e)}")
+            return
+    else:
+        try:
+            path = await bot.download_media(message=file, progress=progress_for_pyrogram, progress_args=("`Downloading...`", ms, c_time))
+        except Exception as e:
+            neg_used = used - int(file.file_size)
+            used_limit(update.from_user.id, neg_used)
+            await ms.edit(str(e))
+            return
+
     splitpath = path.split("/downloads/")
     dow_file_name = splitpath[1]
     old_file_name = f"downloads/{dow_file_name}"
@@ -93,17 +99,18 @@ async def vid(bot, update):
     try:
         c_caption = data[1]
     except:
-        pass
+        c_caption = None
     thumb = data[0]
 
     duration = 0
     metadata = extractMetadata(createParser(file_path))
-    if metadata.has("duration"):
+    if metadata and metadata.has("duration"):
         duration = metadata.get('duration').seconds
     if c_caption:
         vid_list = ["filename", "filesize", "duration"]
         new_tex = escape_invalid_curly_brackets(c_caption, vid_list)
-        caption = new_tex.format(filename=new_filename, filesize=humanbytes(file.file_size), duration=timedelta(seconds=duration))
+        caption = new_tex.format(filename=new_filename, filesize=humanbytes(
+            file.file_size), duration=timedelta(seconds=duration))
     else:
         caption = f"**{new_filename}**"
     if thumb:
@@ -125,7 +132,7 @@ async def vid(bot, update):
     if value < file.file_size:
         await ms.edit("`Trying to upload...`")
         try:
-            filw = await app.send_video(log_channel, video=file_path, thumb=ph_path, duration=duration, caption=caption, progress=progress_for_pyrogram, progress_args=("`Trying to upload...`", ms, c_time))
+            filw = await app.send_video(log_channel, video=file_path, thumb=ph_path, duration=duration, caption=caption, progress=progress_for_pyrogram, progress_args=("`Uploading...`",  ms, c_time))
             from_chat = filw.chat.id
             mg_id = filw.id
             time.sleep(2)
@@ -139,7 +146,7 @@ async def vid(bot, update):
         except Exception as e:
             neg_used = used - int(file.file_size)
             used_limit(update.from_user.id, neg_used)
-            await ms.edit(e)
+            await ms.edit(str(e))
             os.remove(file_path)
             try:
                 os.remove(ph_path)
@@ -149,7 +156,16 @@ async def vid(bot, update):
         await ms.edit("`Trying to upload...`")
         c_time = time.time()
         try:
-            filw = await bot.send_video(log_channel, video=file_path, thumb=ph_path, duration=duration, caption=caption, progress=progress_for_pyrogram, progress_args=("`Trying to upload...`", ms, c_time))
+            filw = await bot.send_video(
+                log_channel,
+                video=file_path,
+                thumb=ph_path,
+                duration=duration,
+                caption=caption,
+                progress=progress_for_pyrogram,
+                progress_args=("`Uploading...`", ms, c_time)
+            )
+
             from_chat = filw.chat.id
             mg_id = filw.id
             time.sleep(2)
@@ -161,6 +177,7 @@ async def vid(bot, update):
             used_limit(update.from_user.id, neg_used)
             await ms.edit(str(e))
             os.remove(file_path)
-            return
-
-# Add any additional code or handlers as needed
+            try:
+                os.remove(ph_path)
+            except:
+                return
